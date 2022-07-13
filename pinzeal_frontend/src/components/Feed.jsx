e27c49ed0b1112161e1863b5100aa3304c17ff49
Priyansh Sharma <96904296+Priyanshsharma21@users.noreply.github.com>
2022-07-14 01:09:39 +0530

    Update Feed.jsx

--- pinzeal_frontend/src/components/Feed.jsx
+++ pinzeal_frontend/src/components/Feed.jsx
@@ -1,46 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
 
-// import { client } from '../client';
-// import { feedQuery, searchQuery } from '../utils/data';
-// import MasonryLayout from './MasonryLayout';
-// // the layout we are trying to achive is masonary layout random patten height and width grid
-// import Spinner from './Spinner';
-
-
-// const Feed = () => {
-//   const [loading, setLoading] = useState(false)
-//   const [pins, setPins] = useState()
-//   const {categoryId} = useParams();
-
-//   useEffect(()=>{
-//     if(categoryId){
-//       setLoading(true)
-//       //when searching for a specific catogery
-//       const query = searchQuery(categoryId);
-//       client.fetch(query)
-//       .then((data)=>{
-//         setPins(data)
-//         setLoading(false)
-//       })
-//     }else{
-//       //if we are on feed means all
-//         client.fetch(feedQuery).then((data)=>{
-//           setPins(data)
-//           setLoading(false)
-//         })
-//     }
-//   },[categoryId])
-
-//   if(loading) return <Spinner message="We are adding new ideas to your feed"/>
-//   return (
-//     <div>
-//     {pins && <MasonryLayout pins={pins}/>}
-//     </div>
-//   )
-// }
-
-// export default Feed
 
 
 import React, { useState, useEffect } from 'react';
